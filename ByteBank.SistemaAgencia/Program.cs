d620d16bb754faadbcf12123e70155203aa5c05b
Guilherme Machado <guifermach@hotmail.com>
2022-05-02 21:58:58 -0300

    Adicionando classes no projeto de  biblioteca

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 41236);
+            Console.WriteLine(conta.Numero);
+            Console.ReadLine();
+        }
+    }
+}
